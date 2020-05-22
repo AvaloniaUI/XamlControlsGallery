3d4abb09ea619de0b7e9fe4e65b61bb55d48141d
Dan Walmsley <dan@walms.co.uk>
2020-05-21 22:46:13 -0300

    initial implementation

---
+++ XamlControlsGallery/FluentWindow.cs
@@ -0,0 +1,35 @@
+﻿using Avalonia.Styling;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Avalonia.Platform;
+using Avalonia.Controls.Primitives;
+
+namespace Avalonia.Controls
+{
+    public class FluentWindow : Window, IStyleable
+    {
+        Type IStyleable.StyleKey => typeof(Window);
+
+        public FluentWindow()
+        {
+            ExtendClientAreaToDecorationsHint = true;
+            ExtendClientAreaChromeHints = ExtendClientAreaChromeHints.SystemChromeButtons;
+            ExtendClientAreaTitleBarHeightHint = -1;
+
+            TransparencyLevelHint = WindowTransparencyLevel.Blur;
+
+            this.GetObservable(WindowStateProperty)
+                .Subscribe(x =>
+                {
+                    PseudoClasses.Set(":maximized", x == WindowState.Maximized);
+                    PseudoClasses.Set(":fullscreen", x == WindowState.FullScreen);
+                });
+        }
+
+        protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+        {
+            base.OnApplyTemplate(e);            
+        }
+    }
+}
---
+++ XamlControlsGallery/Views/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using System;
+
+namespace XamlControlsGallery.Views
+{
+    public class MainWindow : FluentWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var closeButton = this.FindControl<Panel>("PART_CloseButton");
+            var restoreButton = this.FindControl<Panel>("PART_RestoreButton");
+            var minimiseButton = this.FindControl<Panel>("PART_MinimiseButton");
+            var fullScreenButton = this.FindControl<Panel>("PART_FullScreenButton");
+
+            closeButton.PointerPressed += (sender, e) => Close();
+            restoreButton.PointerPressed += (sender, e) => WindowState = WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+            minimiseButton.PointerPressed += (sender, e) => WindowState = WindowState.Minimized;
+            fullScreenButton.PointerPressed += (sender, e) => WindowState = WindowState == WindowState.FullScreen ? WindowState.Normal : WindowState.FullScreen;
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
